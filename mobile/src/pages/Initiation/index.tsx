4e9517b8a799c53c95e0dda94dd7f72b738e7f0c
LuizBoas <luiz.boas@ccc.ufcg.edu.br>
2020-10-19 22:48:39 -0300

    template initiation

--- mobile/src/pages/Initiation/index.tsx
+++ mobile/src/pages/Initiation/index.tsx
@@ -1,12 +1,101 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet, ImageBackground, Image, TouchableOpacity} from "react-native";
+
 
 const Initiation = () => {
     return( 
-        <View> 
-            <Text>Testttttttttttt</Text>
-        </View>
+        <ImageBackground 
+            source={require('../../asserts/PNG/Background.png')} 
+            style={styles.container}
+            
+        >
+            <View style={styles.main}>
+                <Image 
+                    source={require('../../asserts/PNG/Fala.png')}
+                    style={styles.image}
+                />
+                <View>
+                    <Text style={styles.title}>Seja bem-vindo! </Text>
+                    <Text style={styles.description}>Faça sua voz ser ouvida e vamos em busca de uma cidade melhor!</Text>
+                </View>
+                <TouchableOpacity style={styles.button} onPress={()=> {}}>
+                    <Text style={styles.buttonText}>Acessar Conta</Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity onPress={()=> {}}>
+                        <Text style={styles.linkText} >Deseja realizar cadastro?</Text>
+                </TouchableOpacity>
+
+
+            </View>
+        </ImageBackground >
     );
 };
 
 export default Initiation;
+
+const styles = StyleSheet.create({
+   
+    container: {
+        resizeMode: "contain",
+        flex: 1,
+        backgroundColor: '#ffffff',
+    },
+
+    main: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        marginTop: "8%",
+        resizeMode: "contain",
+        flex: 1,  
+        maxHeight: "30%",
+        maxWidth: "94%"
+    },
+
+    title: {
+        color: '#322153',
+        fontSize: 32,
+        fontFamily: 'Ubuntu_700Bold',
+        maxWidth: 260,
+        marginTop: '16%',
+    },
+    
+    description: {
+        color: '#6C6C80',
+        fontSize: 16,
+        marginTop: 16,
+        fontFamily: 'Roboto_400Regular',
+        maxWidth: 260,
+        lineHeight: 24,
+    },
+
+    button: {
+        backgroundColor: '#0B6E4F',
+        height: 60,
+        width: '80%',
+        borderRadius: 20,
+        alignItems: 'center',
+        marginTop: '16%',
+        justifyContent: 'center',
+        
+    },
+  
+    buttonText: {
+        color: '#FFF',
+        fontFamily: 'Roboto_500Medium',
+        fontSize: 21,
+        
+    },
+    linkText: {
+        color: '#5B7D6C',
+        fontFamily: 'Roboto_500Medium',
+        fontSize: 16,
+        marginTop: '8%',
+    }
+
+   
+});
+
