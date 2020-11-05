7505aac3b1dbf547296bb696a275bde166ef24d1
LuizBoas <luiz.boas@ccc.ufcg.edu.br>
2020-11-05 16:40:58 -0300

    save

--- src/pages/Drawer/index.tsx
+++ src/pages/Drawer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
 
@@ -15,58 +15,48 @@ import DrawerContent from '../../components/DrawerContent';
 const DrawerNavigator = createDrawerNavigator();
 
 const Drawer: React.FC = () => {
-  return (
-    <DrawerNavigator.Navigator initialRouteName="Map" drawerContent={(props) => <DrawerContent {...props} />}>
-        
-        <DrawerNavigator.Screen 
-            name="Map" 
-            component={Map}
-            options={{
-                drawerLabel: "Olá Cidadão!",
-                drawerIcon: ({color, size}) => <FontAwesome name="map-marker" size={size} color={color} />,
-            }} 
-        />
-        <DrawerNavigator.Screen
-            name="Reclamações próximas"
-            component={ComplaintClose} 
-        />
-        <DrawerNavigator.Screen 
-            name="Minhas reclamações"
-            component={MyComplaint} 
-        />
-        <DrawerNavigator.Screen
-            
-             
-            name="Editar Perfil"
-            component={EditPerfil} 
-        />
-        <DrawerNavigator.Screen
-        
-            name="Sair"
-            component={Logon}
-            options={{
+    return (
+  
+        <DrawerNavigator.Navigator initialRouteName="Map" drawerContent={(props) => <DrawerContent {...props} />}>
 
-                
-                
-                drawerLabel: "Sair",
-                drawerIcon: ({}) => <MaterialCommunityIcons name="logout" size={24} color="black" />,
-            }}
-           
-        />
-    </DrawerNavigator.Navigator>
-  );
+                <DrawerNavigator.Screen 
+                    name="Map" 
+                    component={Map}
+                    options={{
+                        drawerLabel: "Mapa",
+                        drawerIcon: ({color, size}) => <FontAwesome name="map-marker" size={size} color={color} />,
+                    }} 
+                />
+                <DrawerNavigator.Screen
+                    name="Reclamações próximas"
+                    component={ComplaintClose} 
+                />
+                <DrawerNavigator.Screen 
+                    name="Minhas reclamações"
+                    component={MyComplaint} 
+                />
+                <DrawerNavigator.Screen
+                    name="Editar Perfil"
+                    component={EditPerfil} 
+                />
+
+            <DrawerNavigator.Screen
+
+                name="Sair"
+                component={Logon}
+                options={{
+                    drawerLabel: "Sair",
+                    drawerIcon: ({}) => <MaterialCommunityIcons name="logout" size={24} color="black" />,
+                }}
+            
+            />
+        </DrawerNavigator.Navigator>
+    );
 }
 
 export default Drawer;
 
 const styles = StyleSheet.create({
-    description:{
-        marginBottom: 10
-
-    }
-   
   
-
-   
 });
 
