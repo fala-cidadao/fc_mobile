a6679577644f39f34a6af1e0bb76e46471a5a710
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2020-11-27 14:03:20 -0300

    correções

--- src/pages/DetailComplaints/index.tsx
+++ src/pages/DetailComplaints/index.tsx
@@ -11,17 +11,19 @@ import api from '../../services/api';
 interface ILocation {
     latitude: number;
     longitude: number;
-};
+}
 interface IProblem {
     _id: string;
     adminImages: [];
     category: string;
-    comments: [{
-        owner: string | undefined;
-        text: string;
-        role: string | undefined;
-        image: string;
-    }];
+    comments: [
+        {
+            owner: string | undefined;
+            text: string;
+            role: string | undefined;
+            image: string;
+        },
+    ];
     createdAt: string;
     description: [];
     location: ILocation;
@@ -30,7 +32,7 @@ interface IProblem {
     title: string;
     updateAt: string;
     userImages: string[];
-};
+}
 
 interface Route {
     problem: IProblem;
@@ -52,15 +54,16 @@ const DetailComplaints: React.FC = () => {
             owner: user?.user.userId,
             text: comment,
             role: user?.user.role,
-            image: ''
+            image: '',
         };
 
         problem.comments.push(data);
+        setComment('');
 
         await api.post(`/problem/${problem._id}/comment`, data);
 
         Alert.alert('Comentário adicionado', '', [{ text: 'Ok' }]);
-    }    
+    }
 
     return (
         <ScrollView
@@ -80,26 +83,20 @@ const DetailComplaints: React.FC = () => {
             </View>
             <View style={styles.imagesContainer}>
                 <ScrollView horizontal pagingEnabled>
-                    {
-                        problem.userImages.map((image, index) => {
-                            return (
-                                <Image
-                                    key={index + image}
-                                    style={styles.image}
-                                    source={{
-                                        uri: image,
-                                    }}
-                                />
-                            );
-                        })
-                    }
+                    {problem.userImages.map((image, index) => (
+                        <Image
+                            key={index + image}
+                            style={styles.image}
+                            source={{
+                                uri: image,
+                            }}
+                        />
+                    ))}
                 </ScrollView>
             </View>
 
             <View style={styles.detailsContainer}>
-                <Text style={styles.description}>
-                    {problem.description}
-                </Text>
+                <Text style={styles.description}>{problem.description}</Text>
 
                 <View style={styles.locationContainer}>
                     <Entypo name="location" size={24} color="#0B6E4F" />
@@ -139,19 +136,15 @@ const DetailComplaints: React.FC = () => {
 
             <View style={styles.imagesContainerRes}>
                 <ScrollView horizontal pagingEnabled>
-                    {
-                        problem.adminImages.map((image, index) => {
-                            return (
-                                <Image
-                                    key={index + image}
-                                    style={styles.imageRes}
-                                    source={{
-                                        uri: image
-                                    }}
-                                />
-                            );
-                        })
-                    }
+                    {problem.adminImages.map((image, index) => (
+                        <Image
+                            key={index + image}
+                            style={styles.imageRes}
+                            source={{
+                                uri: image,
+                            }}
+                        />
+                    ))}
                 </ScrollView>
             </View>
 
@@ -184,36 +177,42 @@ const DetailComplaints: React.FC = () => {
                     placeholderTextColor="#8DA1B9"
                     style={styles.input}
                 />
-                <TouchableOpacity onPress={() => handleAddComment(problem)} style={styles.iconPlane}>
+                <TouchableOpacity
+                    onPress={() => handleAddComment(problem)}
+                    style={styles.iconPlane}
+                >
                     <FontAwesome name="paper-plane" size={24} color="#0B6E4F" />
                 </TouchableOpacity>
             </View>
 
-            {
-                problem.comments.map((comment: { owner: string | undefined, text: string, role: string | undefined, image: string }, index) => {
-                    return (
-                        <View key={index + String(comment.owner)} style={styles.headerComments}>
-                            <View style={styles.photoContaner}>
-                                {
-                                    comment.image !== '' && 
-                                    <Image
-                                        style={styles.photo}
-                                        source={{
-                                            uri: comment.image
+            {problem.comments.map(
+                (
+                    comment: {
+                        owner: string | undefined;
+                        text: string;
+                        role: string | undefined;
+                        image: string;
+                    },
+                    index,
+                ) => (
+                    <View key={index + String(comment.owner)} style={styles.headerComments}>
+                        <View style={styles.photoContaner}>
+                            {comment.image !== '' && (
+                                <Image
+                                    style={styles.photo}
+                                    source={{
+                                        uri: comment.image,
                                     }}
                                 />
-                                }
-                            </View>
-                            <View>
-                                <Text style={styles.perfilText}>{comment.role}</Text>
-                                <Text style={styles.perfilDescription}>
-                                    {comment.text}
-                                </Text>
-                            </View>
+                            )}
                         </View>
-                    );
-                })
-            }
+                        <View>
+                            <Text style={styles.perfilText}>{comment.role}</Text>
+                            <Text style={styles.perfilDescription}>{comment.text}</Text>
+                        </View>
+                    </View>
+                ),
+            )}
         </ScrollView>
     );
 };
--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -4,7 +4,7 @@ import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Foundation, Feather } from '@expo/vector-icons';
 import { useNavigation, DrawerActions } from '@react-navigation/native';
 import { FAB, Portal, Provider } from 'react-native-paper';
-import * as Location from 'expo-location'; 
+import * as Location from 'expo-location';
 
 import api from '../../services/api';
 import styles from './styles';
@@ -21,26 +21,26 @@ interface IProblem {
 interface ILocation {
     latitude: number;
     longitude: number;
-};
+}
 
 const Map: React.FC = () => {
     const navigation = useNavigation();
     const [openFilter, setOpenFilter] = useState(false);
-    const [initialPosition,setInitialPosition] = useState<[number, number]>([0, 0]);
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
     const [problems, setProblem] = useState<IProblem[]>([]);
     const [filteredProblems, setFilteredProblems] = useState<IProblem[]>([]);
 
     const filtros: { [label: string]: string } = {
-        "safety": 'Segurança',
-        "energy": 'Energia e Iluminação',
-        "education": 'Educação',
-        "garbage": 'Coleta de Lixo',
-        "health": 'Saúde',
-        "infrastruture": 'Infraestrutura e Mobilidade',
-        "sewer": 'Saneamento Básico e Fornecimento de água',
-        "other": 'Outro'
+        safety: 'Segurança',
+        energy: 'Energia e Iluminação',
+        education: 'Educação',
+        garbage: 'Coleta de Lixo',
+        health: 'Saúde',
+        infrastruture: 'Infraestrutura e Mobilidade',
+        sewer: 'Saneamento Básico e Fornecimento de água',
+        other: 'Outro',
     };
-    
+
     const options = [
         'Segurança',
         'Energia e Iluminação',
@@ -53,45 +53,41 @@ const Map: React.FC = () => {
     ];
 
     const markerColors: Record<string, string> = {
-        'safety': '#DE5F61',
-        'energy': '#171C21',
-        'education': '#009FB7',
-        'garbage': '#648DE5',
-        'health': '#7E1946',
-        'infraestruture': '#FCD757',
-        'sewer': '#0C6291',
-        'other': '#00A878',
-      };
+        safety: '#DE5F61',
+        energy: '#171C21',
+        education: '#009FB7',
+        garbage: '#648DE5',
+        health: '#7E1946',
+        infraestruture: '#FCD757',
+        sewer: '#0C6291',
+        other: '#00A878',
+    };
 
     useEffect(() => {
-        async function loadPosition(){
+        async function loadPosition() {
             const { status } = await Location.requestPermissionsAsync();
-  
-            if (status !== 'granted'){
-              Alert.alert('Oooops....', 'Precisamos de sua permissão para obter a localização')
-              return;
+
+            if (status !== 'granted') {
+                Alert.alert('Oooops....', 'Precisamos de sua permissão para obter a localização');
+                return;
             }
-  
+
             const location = await Location.getCurrentPositionAsync();
-  
-            const { latitude, longitude } = location.coords; 
-  
-            setInitialPosition([
-                latitude,
-                longitude,
-            ]);
+
+            const { latitude, longitude } = location.coords;
+
+            setInitialPosition([latitude, longitude]);
         }
-  
+
         loadPosition();
-        
-      } ,[]);
-  
+    }, []);
+
     useEffect(() => {
-        api.get('problem').then(response => {
-          setProblem(response.data);
-          setFilteredProblems(response.data);
+        api.get('problem').then((response) => {
+            setProblem(response.data);
+            setFilteredProblems(response.data);
         });
-    }, []); 
+    }, []);
 
     function handleNavigateToRegisterComplaints() {
         navigation.navigate('SelectMap');
@@ -106,16 +102,13 @@ const Map: React.FC = () => {
     }
 
     function search(area: string) {
-        const filteredProblems = problems.filter(problem => {
+        const newProblems = problems.filter((problem) => {
             console.log(problem.category, filtros[problem.category], area);
             console.log(filtros[problem.category] === area);
             return filtros[problem.category] === area;
         });
-        setProblem(filteredProblems);
+        setFilteredProblems(newProblems);
     }
-
-    
-       
 
     return (
         <Provider>
@@ -132,22 +125,32 @@ const Map: React.FC = () => {
                                 longitudeDelta: 0.008,
                             }}
                         >
-                        {filteredProblems.map(problem => (
-                            <Marker
-                                key ={String(problem._id)}
-                                coordinate={{
-                                    latitude: Number(problem.location.latitude),
-                                    longitude: Number(problem.location.longitude),
-                                }}
-                                onPress={() => handleNavigateToDetailComplaints(problem)}
-                            >   
-                                <View style={styles.mapMarkerContainer}>
-                                    <Text style={[styles.mapMarkerTitle, { color: markerColors[problem.category] }]}>{problem.title}</Text>
-                                    <Foundation name="marker" size={70} color={markerColors[problem.category]} />
-                                </View>
-                                
-                            </Marker>
-                        ))}
+                            {filteredProblems.map((problem) => (
+                                <Marker
+                                    key={String(problem._id)}
+                                    coordinate={{
+                                        latitude: Number(problem.location.latitude),
+                                        longitude: Number(problem.location.longitude),
+                                    }}
+                                    onPress={() => handleNavigateToDetailComplaints(problem)}
+                                >
+                                    <View style={styles.mapMarkerContainer}>
+                                        <Text
+                                            style={[
+                                                styles.mapMarkerTitle,
+                                                { color: markerColors[problem.category] },
+                                            ]}
+                                        >
+                                            {problem.title}
+                                        </Text>
+                                        <Foundation
+                                            name="marker"
+                                            size={70}
+                                            color={markerColors[problem.category]}
+                                        />
+                                    </View>
+                                </Marker>
+                            ))}
                         </MapView>
                     )}
                     <View style={styles.topBar}>
